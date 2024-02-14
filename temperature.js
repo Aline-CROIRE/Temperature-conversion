72892c586ce796e1780d177776723c58af31b10e
Aline NIYONIZERA <nikyocroirealine@gmail.com>
2024-02-14 03:22:21 -0800

    This is a js to change temperature

---
+++ temperature.js
@@ -0,0 +1,19 @@
+// decalaring constant variable kelvin
+let kelvin=0 ;
+// substracting 273 from value of kelvin
+let celsius= kelvin -273;
+//converting celsius to fahrenheit
+let fahrenheit=celsius*(9/5)+32;
+//rounding fahrenheit value down
+fahrenheit=Math.floor(fahrenheit);
+//displaying fahrenheit temperature
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+//converting celicius to Newton scale
+let newton=celsius*(33/100);
+//rounding down newton
+newton=Math.floor(newton);
+//displaying temperature in newton
+console.log(`The temperature is ${newton} degrees newton scale`);
+
+
+
